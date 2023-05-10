158a962006820e7543bf25deabc7a161d5255e33
obifemi <54670899+obifemi@users.noreply.github.com>
2023-05-10 15:35:46 +0000

    add socket cloud

--- frontend/src/stores/socket.js
+++ frontend/src/stores/socket.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import { io } from 'socket.io-client'
 
 export const socket = io(
-  process.env.NODE_ENV === 'production' ? 'https://api.mertin.info' : 'http://127.0.0.1:3000'
+  import.meta.env.NODE_ENV === 'production' ? 'https://api.mertin.info' : 'http://127.0.0.1:3000'
 )
 
 export const useSocketStore = defineStore('Socket', {
