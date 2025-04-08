6f55de092e26e199d6fd801cde43233a5d7173c1
ali mert inal <mert.inal@gmail.com>
2025-04-08 19:49:15 +0200

    openai

--- backend/src/lib/openai.js
+++ backend/src/lib/openai.js
@@ -1,24 +1,24 @@
-const { Configuration, OpenAIApi } = require('openai')
+// const { Configuration, OpenAIApi } = require('openai')
 
-require('dotenv').config()
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-})
-const openai = new OpenAIApi(configuration)
+// require('dotenv').config()
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// })
+// const openai = new OpenAIApi(configuration)
 
-module.exports = async function (name) {
-  this.name = name
-  const response = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `Create a description for the language challenge by the following information:
-    Challenge Name: ${name}
-    `,
-    temperature: 0.5,
-    max_tokens: 60,
-    top_p: 1.0,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-  })
-  console.log({ name }, response.data.choices[0].text)
-  return response.data.choices[0].text || 'No response'
-}
+// module.exports = async function (name) {
+//   this.name = name
+//   const response = await openai.createCompletion({
+//     model: 'text-davinci-003',
+//     prompt: `Create a description for the language challenge by the following information:
+//     Challenge Name: ${name}
+//     `,
+//     temperature: 0.5,
+//     max_tokens: 60,
+//     top_p: 1.0,
+//     frequency_penalty: 0.0,
+//     presence_penalty: 0.0,
+//   })
+//   console.log({ name }, response.data.choices[0].text)
+//   return response.data.choices[0].text || 'No response'
+// }
