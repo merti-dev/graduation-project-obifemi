dfc08add99da13d9fdee6498fd1f4b7c0500c560
ali mert inal <mert.inal@gmail.com>
2025-04-14 16:50:07 +0200

    docker and vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 5173,
+    host: true
+  },
   plugins: [vue()],
   resolve: {
     alias: {
